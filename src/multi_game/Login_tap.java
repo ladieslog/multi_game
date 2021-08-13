efea747d527862567b4ab4bdf2f19de18a391c0b
matia005 <matia0529@gmail.com>
2021-08-13 13:05:57 +0900

    내 코드 2번째 업로드

--- src/multi_game/Login_tap.java
+++ src/multi_game/Login_tap.java
@@ -1,8 +1,25 @@
 package multi_game;
 
+import java.util.Scanner;
+
 public class Login_tap {
-	public void login() {
-		System.out.println("test");
-		
+	Scanner input =new Scanner(System.in);
+	private String idNum="kim", psNum="ps1234";
+	public int  login() {
+		String id, ps;
+		System.out.println("넵 로그인을 하셔야 합니다. ");
+		System.out.print("id가 뭔가요? : ");
+		id=input.next();
+		System.out.print("password가 뭔가요: ");
+		ps=input.next();
+		if(idNum.equals(id)&&psNum.equals(ps)) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	public void display() {
+		System.out.println("id : "+idNum+"\npassword : "
+				+psNum);
 	}
 }
