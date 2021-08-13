aaf72e8f9a0b37ae81f79ac35694aa647c4bc224
matia005 <matia0529@gmail.com>
2021-08-13 12:57:12 +0900

    내 코드 작성업로드

---
+++ src/multi_game/RandomNumberGame.java
@@ -0,0 +1,44 @@
+package multi_game;
+
+import java.util.Scanner;
+
+public class RandomNumberGame {
+	public void gameStart() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("랜덤 숫자 맞추기 게임입니다.");
+		System.out.println("1 ~ 100까지의 숫자를 입력해주세요.");
+		int randomNum = (int) ((Math.random() * 100) + 1);
+		int count = 0;
+		while(true) {
+			System.out.print("숫자 입력 >>> ");
+			int userNum = scanner.nextInt();
+			int result = numberCheck(userNum, randomNum);
+			switch(result) {
+				case 2:
+					System.out.println("정답입니다!");
+					System.out.println("횟수는 " + count + "번 입니다.");
+					return;
+				case 1:
+					System.out.println("입력하신 숫자가 더 높습니다!");
+					break;
+				case 0:
+					System.out.println("입력하신 숫자가 더 낮습니다!");
+					break;
+				default:
+					break;
+			}
+			count++;
+		}
+	}
+	
+	private int numberCheck(int userNum, int randomNum) {
+		if(userNum == randomNum) {
+			return 2;
+		} else if(userNum > randomNum) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+}
